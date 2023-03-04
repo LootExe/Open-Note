95532fb44db60d261e3424a18ea71d00bf0116c1
LootExe <114713024+LootExe@users.noreply.github.com>
2023-03-04 13:43:17 +1100

    moved to a feature based folder structure.

--- lib/src/bloc/settings_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'settings_bloc.dart';
-
-abstract class SettingsEvent {}
-
-class SettingsLoaded extends SettingsEvent {}
-
-class SettingsChanged extends SettingsEvent {}
--- lib/src/model/note_list_file.dart
+++
@@ -1,6 +0,0 @@
-class NoteListFile {
-  final String name;
-  final Uri uri;
-
-  NoteListFile({required this.name, required this.uri});
-}
