1569c8a84449dc8c2a01fbfe3304cc5b49088ec8
LootExe <114713024+LootExe@users.noreply.github.com>
2023-03-04 13:44:07 +1100

    added general config classes for themes, widget design and general constants.

---
+++ lib/src/config/app_design.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppDesign {
+  static const appBarDateTextStyle = TextStyle(fontSize: 16.0);
+  static const appBarActionPadding = EdgeInsets.only(right: 6.0);
+  static const mainContentPadding = EdgeInsets.only(
+    left: 20.0,
+    top: 20.0,
+    right: 20.0,
+  );
+
+  static const emptyTextFieldDecoration = InputDecoration(
+    contentPadding: EdgeInsets.zero,
+    counterText: '',
+    filled: false,
+    border: InputBorder.none,
+  );
+
+  /// Zero-Width space unicode character.
+  static const kZeroWidthSpace = '\u200b';
+}
