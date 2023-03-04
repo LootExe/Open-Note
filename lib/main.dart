7f45e41a1ed7a063e3da0455b79f07cf90e67e5f
LootExe <114713024+LootExe@users.noreply.github.com>
2023-03-04 13:41:32 +1100

    repositories are created in main now.

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,43 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import 'package:local_settings_provider/local_settings_provider.dart';
+import 'package:note_backup_provider/note_backup_provider.dart';
+import 'package:settings_repository/settings_repository.dart';
+import 'package:local_note_provider/local_note_provider.dart';
+import 'package:note_repository/note_repository.dart';
 
 import 'app.dart';
-import 'src/repository/notes_repository.dart';
-import 'src/repository/settings_repository.dart';
+import 'src/config/app_config.dart';
 
-void main() {
-  GoogleFonts.config.allowRuntimeFetching = false;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
   LicenseRegistry.addLicense(() async* {
-    final license =
-        await rootBundle.loadString('asset/google_fonts/LICENSE.txt');
-    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+    final license = await rootBundle.loadString(AppConfig.fontLicense);
+    yield LicenseEntryWithLineBreaks([AppConfig.fontName], license);
   });
 
-  runApp(
-    MultiRepositoryProvider(
-      providers: [
-        RepositoryProvider<SettingsRepository>(
-          create: (_) => SettingsRepository(),
-        ),
-        RepositoryProvider<NotesRepository>(
-          create: (_) => NotesRepository(),
-        ),
-      ],
-      child: const App(),
-    ),
+  final preferences = await SharedPreferences.getInstance();
+
+  final settingsRepository = SettingsRepository(
+    provider: LocalSettingsProvider(plugin: preferences),
   );
+
+  final notesRepository = NoteRepository(
+    noteProvider: LocalNoteProvider(plugin: preferences),
+    backupProvider: NoteBackupProvider(),
+  );
+
+  // Set system orientation to portrait only.
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(App(
+    settingsRepository: settingsRepository,
+    notesRepository: notesRepository,
+  ));
 }
