fabbd9aa945a5752a212a65302b05997a62f0965
Frank <114713024+LootExe@users.noreply.github.com>
2023-10-10 11:02:46 +1100

    Removed BlocBuilder and used context.watch instead.

--- lib/settings/widget/theme_mode_dialog.dart
+++ lib/settings/widget/theme_mode_dialog.dart
@@ -1,51 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:open_note/common/utils.dart';
 import 'package:open_note/l10n/generated/l10n.dart';
 import 'package:open_note/settings/bloc/settings_bloc.dart';
-import 'package:settings_repository/settings_repository.dart';
 
 class ThemeModeDialog {
   static void show(BuildContext context) {
     showDialog<void>(
       context: context,
-      builder: (context) => AlertDialog(
-        title: Text(S.of(context).themeDialogTitle),
-        content: BlocBuilder<SettingsBloc, SettingsState>(
-          builder: (context, state) => Column(
+      builder: (context) {
+        final l10n = S.of(context);
+        final settings = context.watch<SettingsBloc>().state.settings;
+
+        void onChanged(ThemeMode? value) => context.read<SettingsBloc>().add(
+              SettingsChanged(
+                settings.copyWith(themeMode: value ?? ThemeMode.system),
+              ),
+            );
+
+        return AlertDialog(
+          title: Text(l10n.themeDialogTitle),
+          content: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: _buildRadioGroup(
-              context: context,
-              settings: state.settings,
-            ),
+            children: [
+              RadioListTile(
+                title: Text(l10n.settingsThemeSystem),
+                value: ThemeMode.system,
+                groupValue: settings.themeMode,
+                onChanged: onChanged,
+              ),
+              RadioListTile(
+                title: Text(l10n.settingsThemeLight),
+                value: ThemeMode.light,
+                groupValue: settings.themeMode,
+                onChanged: onChanged,
+              ),
+              RadioListTile(
+                title: Text(l10n.settingsThemeDark),
+                value: ThemeMode.dark,
+                groupValue: settings.themeMode,
+                onChanged: onChanged,
+              ),
+            ],
           ),
-        ),
-      ),
+        );
+      },
     );
-  }
-
-  static List<Widget> _buildRadioGroup({
-    required BuildContext context,
-    required Settings settings,
-  }) {
-    void onChanged(ThemeMode? value) {
-      final changedSettings = settings.copyWith(
-        themeMode: value ?? ThemeMode.system,
-      );
-
-      context.read<SettingsBloc>().add(SettingsChanged(changedSettings));
-    }
-
-    return ThemeMode.values
-        .map(
-          (mode) => RadioListTile(
-            title: Text(getThemeModeText(mode)),
-            groupValue: settings.themeMode,
-            value: mode,
-            onChanged: onChanged,
-          ),
-        )
-        .toList(growable: false);
   }
 }
