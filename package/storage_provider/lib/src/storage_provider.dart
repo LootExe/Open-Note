b9fbe8752c55420c4731913ca6b551279a08dd4d
Frank <114713024+LootExe@users.noreply.github.com>
2023-10-16 13:13:38 +1100

    Added readKeys() method and updated description.

--- package/storage_provider/lib/src/storage_provider.dart
+++ package/storage_provider/lib/src/storage_provider.dart
@@ -5,12 +5,16 @@ abstract class StorageProvider {
   /// {@macro storage_provider}
   const StorageProvider();
 
-  /// Reads value as `String` from the storage with the specified [key].
-  /// Returns a `Future` that completes once the data has been read.
+  /// Reads all keys from the storage.
+  /// Returns a `Future` that completes once the keys have been read.
+  Future<Set<String>?> readKeys();
+
+  /// Reads the value as `String` from the storage with the specified [key].
+  /// Returns a `Future` that completes once the value has been read.
   Future<String?> read(String key);
 
   /// Writes the [value] with the specified [key] to the storage.
-  /// Returns a `Future` that completes once the data has been written.
+  /// Returns a `Future` that completes once the value has been written.
   Future<void> write(String key, String value);
 
   /// Deletes the key value pair from the storage.
