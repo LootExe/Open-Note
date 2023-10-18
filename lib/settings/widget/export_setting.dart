efa81c64f59061b858afa35e0a770e53414943ff
Frank <114713024+LootExe@users.noreply.github.com>
2023-10-18 13:23:20 +1100

    Disabled import / export settings until backup provider is fully integrated.

--- lib/settings/widget/export_setting.dart
+++ lib/settings/widget/export_setting.dart
@@ -1,16 +1,16 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+//import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:open_note/common/extension/extension.dart';
 import 'package:open_note/l10n/generated/l10n.dart';
-import 'package:open_note/note_list/note_list.dart';
+//import 'package:open_note/note_list/note_list.dart';
 import 'package:open_note/settings/widget/widget.dart';
 
 class ExportSetting extends StatelessWidget {
   const ExportSetting({super.key});
 
   Future<void> _onPressed(BuildContext context) async {
-    final bloc = context.read<NoteListBloc>();
+    //final bloc = context.read<NoteListBloc>();
     final messenger = ScaffoldMessenger.of(context);
     final l10n = S.of(context);
     final directory = await FilePicker.platform.getDirectoryPath();
@@ -21,7 +21,7 @@ class ExportSetting extends StatelessWidget {
       return;
     }
 
-    bloc.add(NoteListExported(directory));
+    //bloc.add(NoteListExported(directory));
     messenger.showSnackMessage(l10n.settingExportSuccess);
   }
 
--- lib/settings/widget/import_setting.dart
+++ lib/settings/widget/import_setting.dart
@@ -1,16 +1,16 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+//import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:open_note/common/extension/extension.dart';
 import 'package:open_note/l10n/generated/l10n.dart';
-import 'package:open_note/note_list/note_list.dart';
+//import 'package:open_note/note_list/note_list.dart';
 import 'package:open_note/settings/widget/widget.dart';
 
 class ImportSetting extends StatelessWidget {
   const ImportSetting({super.key});
 
   Future<void> _onPressed(BuildContext context) async {
-    final bloc = context.read<NoteListBloc>();
+    //final bloc = context.read<NoteListBloc>();
     final messenger = ScaffoldMessenger.of(context);
     final l10n = S.of(context);
     final result = await FilePicker.platform.pickFiles(
@@ -26,7 +26,7 @@ class ImportSetting extends StatelessWidget {
       return;
     }
 
-    bloc.add(NoteListImported(filePath));
+    //bloc.add(NoteListImported(filePath));
     messenger.showSnackMessage(l10n.settingImportSuccess);
   }
 
